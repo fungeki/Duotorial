e73ca0bcf5670db77c0630f800e9d565b18b583d
Ran Loock <fang.society@gmail.com>
2018-03-29 12:20:54 +0300

    :)

--- app/src/main/java/productions/ranuskin/meow/duotorial/MainActivity.java
+++ app/src/main/java/productions/ranuskin/meow/duotorial/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.transition.Fade;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,8 +15,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
