203635beba63847c46a2cb6422d975749e5fc639
Ran Loock <fang.society@gmail.com>
2018-03-29 11:23:09 +0300

    :)

--- app/src/main/java/productions/ranuskin/meow/duotorial/MainActivity.java
+++ app/src/main/java/productions/ranuskin/meow/duotorial/MainActivity.java
@@ -1,8 +1,10 @@
 package productions.ranuskin.meow.duotorial;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,34 +14,96 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private ImageView tabBrowse;
+    private ImageView tabFeatured;
+    private ImageView tabMyDuoFragment;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+
+
+    }
+
+    private void initialize() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        tabBrowse= findViewById(R.id.ivBrowse);
+        tabFeatured= findViewById(R.id.ivFeatured);
+        tabMyDuoFragment= findViewById(R.id.ivMyDuo);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        viewPagerChangeListener();
+        mViewPager.setCurrentItem(1);
+    }
+
+    private void viewPagerChangeListener() {
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+
+                switch (position){
+                    case 0:
+                        switchToBrowse();
+                        break;
+                    case 1:
+                        switchToFeatured();
+                        break;
+                    case 2:
+                        switchToMyDuo();
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+
+            }
+        });
+    }
+
+    private void switchToMyDuo() {
+        tabBrowse.setVisibility(View.VISIBLE);
+        tabFeatured.setVisibility(View.VISIBLE);
+        tabMyDuoFragment.setVisibility(View.GONE);
+    }
+
+    private void switchToFeatured() {
+        tabBrowse.setVisibility(View.VISIBLE);
+        tabFeatured.setVisibility(View.GONE);
+        tabMyDuoFragment.setVisibility(View.VISIBLE);
+    }
+
+    private void switchToBrowse() {
+        tabBrowse.setVisibility(View.GONE);
+        tabFeatured.setVisibility(View.VISIBLE);
+        tabMyDuoFragment.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -98,4 +162,54 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void toBrowse(View view) {
+            switchToBrowse();
+            mViewPager.setCurrentItem(0);
+    }
+
+    public void toFeatured(View view) {
+        switchToFeatured();
+        mViewPager.setCurrentItem(1);
+    }
+
+    public void toMyDuo(View view) {
+        switchToMyDuo();
+        mViewPager.setCurrentItem(2);
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+
+        }
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+
+            // comm.response(position);
+            switch (position){
+                case 0:
+                    return new BrowseFragment();
+                case 1:
+                    return new FeaturedFragment();
+                case 2:
+                    return new MyDuoFragment();
+
+            }
+            return null;
+        }
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+    }
+
+
 }
